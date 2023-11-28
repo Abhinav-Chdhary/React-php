660af5ded4cfac5ed734dfe3fe4253b1f790c691
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-11-28 11:19:41 +0530

    File organization and routing

--- react-php-crud/src/App.js
+++ react-php-crud/src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ListUser from "./components/ListUser";
+import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <h2>React CRUD Ops using PHP API and mySQL</h2>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="user/create">CreateUser</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<ListUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
